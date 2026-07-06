821b8b349432790a197980ea3d19450e32c2bf02
jamessu712@126.com <@sc050803@>
2026-07-06 18:59:36 +0800

    add code

--- src/daily-report/config.py
+++ src/daily-report/config.py
@@ -15,5 +15,5 @@ client = SecretClient(vault_url=KV_URI, credential=credential)
 
 AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT", "AZURE_ENDPOINT")
 OPENAI_API_KEY = client.get_secret("openai-api-key").value
-API_VERSION = os.getenv("API_VERSION", "API_VERSION")
+AZURE_API_VERSION = os.getenv("API_VERSION", "API_VERSION")
 AZURE_DEPLOYMENT_NAME = os.getenv("AZURE_DEPLOYMENT_NAME", "AZURE_DEPLOYMENT_NAME")
--- src/secure-rag-agent/main.py
+++ src/secure-rag-agent/main.py
@@ -1,6 +1,7 @@
 # main.py
 from pii_redactor import redact_pii
 from pii_redactor import hash_email
+from pii_redactor import hash_pii
 from vector_store import upsert_document
 from rag_agent import generate_answer
 from rag_agent import rag_QA
@@ -21,11 +22,11 @@ raw_logs = [
 ]
 
 # Redact PII and store in database
-for log in raw_logs:
-    redacted = redact_pii(log, HMAC_SALT)
-    doc_id = str(uuid.uuid4())
-    print(f"doc_id = {doc_id}")
-    upsert_document(doc_id, log, redacted)  # Note: original log is not stored!
+# for log in raw_logs:
+#     redacted = redact_pii(log, HMAC_SALT)
+#     doc_id = str(uuid.uuid4())
+#     print(f"doc_id = {doc_id}")
+#     upsert_document(doc_id, log, redacted)  # Note: original log is not stored!
 
 # print("All logs ingested securely.")
 
@@ -33,21 +34,33 @@ for log in raw_logs:
 # question = "What did user james su do?"
 # rag_QA(question)
 
-# question = "What did user james do?"
+# question = "What did user James Su do?"
 # rag_QA(question)
 
-# === RAG query example ===
-# # rag with unredacted
-# question = "What did user john.doe@company.org do?"
+# question = "What did user james_81@gmail.com do?"
 # rag_QA(question)
-#
-# # rag with hashed email
-# hashed = hash_pii("john.doe@company.org", HMAC_SALT)
+
+# question = "What did user user_34405f0d347eacdb do?"
+# rag_QA(question)
+
+# question = "What did user name_f4adebab7e2bd95e do?"
+# rag_QA(question)
+
+
+# rag with hashed email
+# hashed = hash_pii("james_81@gmail.com", HMAC_SALT)
 # placeholder = f"user_{hashed}"
 # question = f"What did user {placeholder} do?"
 # rag_QA(question)
-#
-# # rag with redacted
-# question = "What did user user_c286a3aeca0af2f3 do?"
-# rag_QA(question)
+
+# rag with hashed name
+hashed = hash_pii("james su", HMAC_SALT)
+placeholder = f"user_{hashed}"
+question = f"What did user {placeholder} do?"
+rag_QA(question)
+
+hashed = hash_pii("James Su", HMAC_SALT)
+placeholder = f"user_{hashed}"
+question = f"What did user {placeholder} do?"
+rag_QA(question)
 
